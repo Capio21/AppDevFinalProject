5917360566a803bb9e7b3a3ed633a2876e2bd646
Capio21 <130652957+Capio21@users.noreply.github.com>
2023-11-08 13:11:25 +0800

    12345

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,107 +1,124 @@
 <template>
-    
+      <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-   
-    <body>
-       
-      <div class="area" ></div>
-      <nav class="main-menu" >
-        <ul>
-          <li>
-            <a href="/">
-              <i class="fa fa-home fa-2x"></i>
-              <span class="nav-text"> 
-                
-                Home Page
-              </span>
-            </a>
-    
-          </li>
-          <li class="has-subnav">
-            <a href="#">
-              <i class="fa fa-globe fa-2x"></i>
-              <span class="nav-text">
-                Global Surveyors
-              </span>
-            </a>
-    
-          </li>
-          <li class="has-subnav">
-            <a href="#">
-              <i class="fa fa-comments fa-2x"></i>
-              <span class="nav-text">
-                Group Hub Forums
-              </span>
-            </a>
-    
-          </li>
-          <li class="has-subnav">
-            <a href="#">
-              <i class="fa fa-camera-retro fa-2x"></i>
-              <span class="nav-text">
-                Survey Photos
-              </span>
-            </a>
-    
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-film fa-2x"></i>
-              <span class="nav-text">
-                Surveying Tutorials
-              </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-book fa-2x"></i>
-              <span class="nav-text">
-                Surveying Jobs
-              </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-cogs fa-2x"></i>
-              <span class="nav-text">
-                Tools & Resources
-              </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-map-marker fa-2x"></i>
-              <span class="nav-text">
-                Member Map
-              </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-info fa-2x"></i>
-              <span class="nav-text">
-                Documentation
-              </span>
-            </a>
-          </li>
-        </ul>
-    
-        <ul class="logout">
-          <li>
-            <a href="/Login">
-              <i class="fa fa-power-off fa-2x"></i>
-              <span class="nav-text">
-                Logout
-              </span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </body>
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-laugh-wink"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+</a>
 
-    
-    </template>
-    
-    <style>
-    @import '../assets/asset/css/admin.css';
-    </style>
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Interface
+</div>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Components</span>
+    </a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Components:</h6>
+            <a class="collapse-item" href="buttons.html">Buttons</a>
+            <a class="collapse-item" href="cards.html">Cards</a>
+        </div>
+    </div>
+</li>
+
+<!-- Nav Item - Utilities Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+        aria-expanded="true" aria-controls="collapseUtilities">
+        <i class="fas fa-fw fa-wrench"></i>
+        <span>Utilities</span>
+    </a>
+    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+        data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Utilities:</h6>
+            <a class="collapse-item" href="utilities-color.html">Colors</a>
+            <a class="collapse-item" href="utilities-border.html">Borders</a>
+            <a class="collapse-item" href="utilities-animation.html">Animations</a>
+            <a class="collapse-item" href="utilities-other.html">Other</a>
+        </div>
+    </div>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    Addons
+</div>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+        aria-expanded="true" aria-controls="collapsePages">
+        <i class="fas fa-fw fa-folder"></i>
+        <span>Pages</span>
+    </a>
+    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Login Screens:</h6>
+            <a class="collapse-item" href="login.html">Login</a>
+            <a class="collapse-item" href="register.html">Register</a>
+            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+            <div class="collapse-divider"></div>
+            <h6 class="collapse-header">Other Pages:</h6>
+            <a class="collapse-item" href="404.html">404 Page</a>
+            <a class="collapse-item" href="blank.html">Blank Page</a>
+        </div>
+    </div>
+</li>
+
+<!-- Nav Item - Charts -->
+<li class="nav-item">
+    <a class="nav-link" href="charts.html">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Charts</span></a>
+</li>
+
+<!-- Nav Item - Tables -->
+<li class="nav-item">
+    <a class="nav-link" href="tables.html">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Tables</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+<!-- Sidebar Message -->
+<div class="sidebar-card d-none d-lg-flex">
+    <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
+    <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
+    <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
+</div>
+
+</ul>
+</template>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,16 +1,26 @@
 <template>
+     <div id = "wrapper">
+       
+        <sidebar/>
+     <topbar/>
      
-    <sidebar/>
+   
+     </div>
     
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import sidebar from '../components/sidebar.vue'
+import topbar from '../components/topbar.vue'
+
 
 export default defineComponent({
  components:{
   sidebar,
+  topbar,
+ 
+  
   
   
   
@@ -18,8 +28,15 @@ export default defineComponent({
  
 created(){
   console.log(sidebar);
+  console.log(topbar);
+  
   
   
 }
 });
 </script>
+<style>
+@import '../assets/asset/admin/css/sb-admin-2.css';
+@import '../assets/asset/admin/css/sb-admin-2.min.css';
+
+</style>
